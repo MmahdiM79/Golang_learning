44f6290f770a8ede83a22f22f216f14607dfa045
Mohammad Mahdi Malmasi <m.mahdi.m79@gmail.com>
2022-10-18 12:19:02 +0330

    clientHandler function added

--- TCP-client-server/server.go
+++ TCP-client-server/server.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+const BUFFER_SIZE = 8192
+
 func main() {
 	args := os.Args[1:]
 
@@ -14,15 +16,38 @@ func main() {
 		panic(err)
 	}
 	defer con.Close()
+	fmt.Printf("Server started on %s ...\n\n", con.Addr())
 
-	conn, err := con.Accept()
+	for {
+		conn, err := con.Accept()
+		if err != nil {
+			panic(err)
+		}
 
-	// read message from connection
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		panic(err)
+		go clientHandler(conn)
 	}
-	fmt.Println(string(buf[:n]))
 }
 
+func clientHandler(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Printf("\nClient connected from: %s\n\n", conn.RemoteAddr(), )
+
+	for {
+		// read message from connection
+		buf := make([]byte, BUFFER_SIZE)
+		n, err := conn.Read(buf)
+		if err != nil {
+			break
+		}
+
+		msg := string(buf[:n])
+		if msg == "bye" {
+			break
+		}
+
+		fmt.Printf("\n[Client %s]: %s\n", conn.RemoteAddr(), msg)
+	}
+
+	fmt.Println("Client", conn.RemoteAddr(), "disconnected")
+}
