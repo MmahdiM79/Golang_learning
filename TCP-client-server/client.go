0c657d718e8c8194306cf3b46771d0f3310e5f45
Mohammad Mahdi Malmasi <m.mahdi.m79@gmail.com>
2022-10-18 13:02:04 +0330

    serverMessages function added

--- TCP-client-server/client.go
+++ TCP-client-server/client.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"bufio"
 )
+
+const BUFFER_SIZE = 8192
 
 func main() {
 	args := os.Args[1:]
@@ -14,11 +17,17 @@ func main() {
 		panic(err)
 	}
 	defer conn.Close()
+	fmt.Printf("Connected to server %s\n\n", conn.RemoteAddr())
 
+	go serverMessages(conn)
 
 	var msg string
+	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Scanf("%s", &msg)
+		fmt.Print(">>> ")
+		msg, _ = reader.ReadString('\n')
+		msg = msg[:len(msg)-1]
+		
 		if msg == "bye" {
 			break
 		}
@@ -26,3 +35,23 @@ func main() {
 		conn.Write([]byte(msg))
 	}
 }
+
+
+func serverMessages(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, BUFFER_SIZE)
+		n, err := conn.Read(buf)
+		if err != nil {
+			break
+		}
+
+		msg := string(buf[:n])
+		if msg == "bye" {
+			fmt.Println("\n\nServer disconnected :( ")
+			os.Exit(0)
+		}
+	}
+}
+
