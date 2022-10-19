e213d62ff5cbb11f62320b0e12c8c931c328407d
Mohammad Mahdi Malmasi <m.mahdi.m79@gmail.com>
2022-10-19 10:25:07 +0330

    Running Multiple Servers at One Time added

--- httpServer/main.go
+++ httpServer/main.go
@@ -2,32 +2,72 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
-	"os"
+	"context"
 	"errors"
 	"io"
 )
+
+const keyServerAddr = "serverAddr"
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe("127.0.0.1:8080", mux)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Println("Server closed")
-	} else if err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
-		os.Exit(1)
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	serverOne := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
 	}
+
+	serverTwo := &http.Server{
+		Addr:    ":8081",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server two: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
 }
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("got / request")
+	ctx := r.Context()
+
+	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "This is my website!\n")
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("got /hello request")
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "Hello, HTTP!\n")
 }
